f50039f784100981f0cf5eefb283ec81aa1c0c67
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-03-02 12:37:21 +0530

    [FEAT] Added a register function

--- server/main.py
+++ server/main.py
@@ -3,6 +3,9 @@ from fastapi.security import OAuth2PasswordBearer
 from databases import Database as Db
 from sqlalchemy import create_engine
 from formats import User, Queries
+from string import ascii_lowercase, ascii_uppercase, hexdigits, octdigits
+from random import choice
+from hashlib import sha256
 
 app = FastAPI()
 DATABASE_URL = "mysql+pymysql://root:override@localhost/app"
@@ -31,3 +34,21 @@ async def exists(username: str):
         return True
     else:
         return False
+
+
+@app.get("/users/{username}/register")
+async def register(username: str, email: str, password: str):
+    if not await exists(username):
+        salt1 = ''.join([choice(choice([ascii_uppercase, ascii_lowercase, hexdigits, octdigits])) for _ in range(512)])
+        salt2 = ''.join([choice(choice([ascii_uppercase, ascii_lowercase, hexdigits, octdigits])) for _ in range(512)])
+        hashed_pass = sha256(''.join([salt1, password, salt2]).encode('utf-8')).hexdigest()
+        await database.execute(queries.USER_SETUP, {
+            "username": username,
+            "email": email,
+            "password_hash": hashed_pass,
+            "salt1": salt1,
+            "salt2": salt2
+        })
+        return "Success!"
+    else:
+        return "User Already Exists!"

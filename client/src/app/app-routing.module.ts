5361d0f45a30b1088c2be2c8702ef2eebab7042b
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-03-03 22:57:33 +0530

    [FIX] Made 'app.module.ts' declare all the components Instead of having a module and routing file for each component, I made app.module.ts declare all of them

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {HomePage} from "./home/home.page";
+import {LoginPage} from "./login/login.page";
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    component: HomePage
+  },
+  {
+    path: 'login',
+    component: LoginPage
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
 ];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,9 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {LoginPage} from "./login/login.page";
+import {HomePage} from "./home/home.page";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginPage, HomePage],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],

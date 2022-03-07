125000adf0afeb41e48c6cc94a465d450e087930
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-03-07 12:39:07 +0530

    [FEAT] Added an artists describe method

--- server/main.py
+++ server/main.py
@@ -422,6 +422,26 @@ async def get_song(name: str, user: User = Depends(user_login)):
         return result
 
 
+@app.get("/artists/get")
+async def get_artist(name: str):
+    id = (await database.fetch_one(f"SELECT id FROM songs.artists WHERE name = '{name}'"))[0]
+    return [
+        await database.fetch_all(f"""
+            SELECT artists.name AS name,
+                artists.followers AS followers
+            FROM songs.artists
+            WHERE id = {id};
+        """),
+        await database.fetch_all(f"""
+            SELECT DISTINCT album AS albums FROM songs.songs WHERE artist_id = {id};
+        """),
+        await database.fetch_all(f"""
+            SELECT name, listen_count, album, genre FROM songs.songs AS top_songs WHERE artist_id = {id} ORDER BY listen_count LIMIT 5;
+        """),
+        await database.fetch_one(f"SELECT SUM(listen_count) AS total_listens FROM songs.songs WHERE artist_id = {id}")
+    ]
+
+
 @app.get("/autocomplete")
 async def return_results(query: str):
     ops = []
